4cfff969e957b34701cd132b95aba26b5045ccc0
David Dassau <todieahero@hotmail.com>
2018-02-24 13:57:53 -0600

    still working on 1

---
+++ ChinookConsole/DataAccess/InvoiceQuery.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ChinookConsole.DataAccess
+{
+    class InvoiceQuery
+    {
+        readonly string _connectionString = ConfigurationManager.ConnectionStrings["Chinook"].ConnectionString;
+
+        public List<Invoice> GetInvoiceWithSalesAgentName()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"select e.FirstName + ' ' + e.LastName Name, i.InvoiceId
+                                    from Employee e
+                                        join customer c on c.SupportRepId = e.EmployeeId
+                                        join invoice i on i.CustomerId = c.CustomerId";
+
+
+
+                var reader = cmd.ExecuteReader();
+
+                var invoices = new List<Invoice>();
+
+                while (reader.Read())
+                {
+                    var invoice = new Invoice
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
