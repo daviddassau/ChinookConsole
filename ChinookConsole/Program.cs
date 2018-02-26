378e3573035b704823f96ce1fa87b55983e81ea3
David Dassau <todieahero@hotmail.com>
2018-02-26 10:46:50 -0600

    completed number 1

--- ChinookConsole/Program.cs
+++ ChinookConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChinookConsole.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,21 @@ namespace ChinookConsole
     {
         static void Main(string[] args)
         {
-            
+            var invoiceQuery = new InvoiceQuery();
+
+            var invoices = invoiceQuery.GetInvoiceWithSalesAgentName();
+
+            Console.WriteLine("Here are all the Sales Agents with their respective Invoice IDs");
+
+            foreach (var invoice in invoices)
+            {
+                Console.WriteLine($"Sales Agent: {invoice.Name}, Invoice Id: {invoice.InvoiceId}");
+            }
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
